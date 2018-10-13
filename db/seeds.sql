826acb37cb4dc12c595403e95a1ed98df7409a4f
showel22 <schowel22@gmail.com>
2018-10-13 12:21:42 -0400

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO cats (name) VALUES ('Meatstick', true);
+INSERT INTO cats (name) VALUES ('Holeman and Finch', true);
+INSERT INTO cats (name, sleepy) VALUES ('TGM');
+INSERT INTO cats (name, sleepy) VALUES ('Bocado');
